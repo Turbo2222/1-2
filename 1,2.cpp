be44be66018e1fa794334d238c8af2a9dc4f973d
Asa <gibiblik@mail.ru>
2024-03-04 23:33:34 +0300

    Добавьте файлы проекта.

---
+++ 1,2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+struct person {
+    int score;
+    std::string name;
+    int balance;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    person p;
+    std::cout << "Введите номер счета:";
+    std::cin >> p.score;
+    std::cout << "Введите имя владельца:";
+    std::cin >> p.name;
+    std::cout << "Введите баланс владельца:";
+    std::cin >> p.balance;
+    std::cout << "Введите новый баланс владельца;";
+    std::cin >> p.balance;
+    std::cout << p.name << "," << p.score << "," << p.balance;
+
+    return 0;
+}
