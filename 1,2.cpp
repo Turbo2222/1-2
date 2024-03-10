fe52a4172b68139c793728c031a562d6a85a3033
Asa <gibiblik@mail.ru>
2024-03-10 13:19:14 +0300

    Добавили функцию для нового баланса.

--- 1,2.cpp
+++ 1,2.cpp
@@ -6,6 +6,12 @@ struct person {
     int balance;
 };
 
+void new_balance(person& pers){
+    pers.balance;
+    
+
+}
+
 int main()
 {
     setlocale(LC_ALL, "RUSSIAN");
@@ -16,9 +22,11 @@ int main()
     std::cin >> p.name;
     std::cout << "Введите баланс владельца:";
     std::cin >> p.balance;
-    std::cout << "Введите новый баланс владельца;";
+ 
+    std::cout << "Введите новый баланс владельца:";
+    new_balance(p);
     std::cin >> p.balance;
-    std::cout << p.name << "," << p.score << "," << p.balance;
+    std::cout << "Ваш счет: " << p.name << "," << p.score << "," << p.balance;
 
     return 0;
 }
